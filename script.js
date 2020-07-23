117883338eb98dbca6e40164a5fb8828c98d79ef
Chris Taylor <chrismtaylor1998@gmail.com>
2020-07-23 14:28:53 -0400

    (FEATURE) Added changing input boxes when clicking the random color button

--- script.js
+++ script.js
@@ -12,9 +12,13 @@ function setGradient(c1, c2) {
 	+ ", " 
 	+ c2
 	+ ")";
-
-
 	css.textContent = body.style.background + ";";
+}
+//update input color when the background is changed
+function setInputColor(c1, c2){
+	color1.value = c1;
+	console.log(color1.value);
+	color2.value = c2;
 }
 
 // call function on startup
@@ -26,11 +30,32 @@ function random_rgb() {
     return 'rgb(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ')';
 }
 
+// Create a function to change an rgb value ( rbg(50,60,70) ) to HEX
+function rgbToHex(rgbColor){
+
+	rgbColor = rgbColor.slice(4, -1);
+	var rgbColorAry = rgbColor.split(",");
+
+	// changing each number in ary to hex
+	var hexColorAry = rgbColorAry.map(function(item){
+		item = parseInt(item).toString(16);
+		return (item.length==1) ? "0" + item : item; // if item is length 1 return 0 + "item" else return item
+	})
+	var hexColor = "#" + hexColorAry.join("");
+
+	return hexColor;
+}
+
+
 //Create a random button to assign two random colors to each input
 button.addEventListener("click", function(){
 	random1 = random_rgb();
 	random2 = random_rgb();
 	setGradient(random1, random2);
+
+	// change the input colors when clicking the random button
+	setInputColor(rgbToHex(random1), rgbToHex(random2));
+
 
 })
 
